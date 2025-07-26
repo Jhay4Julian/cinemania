40146758a19c94dcf8d75e681f9883fd3a685031
Julian Koripamo <jkoripamo@gmail.com>
2025-07-26 18:33:30 +0100

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import MovieDetails from './pages/MovieDetails'
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <div className='min-h-screen bg-gray-100 text-gray-900'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movie/:id' element={<MovieDetails />} />
+          <Route path='*' element={<div className='p-10'>404 - Page Not Found</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
