8466f43da460e9844adeb5c0769b512b4d190e47
Julian Koripamo <jkoripamo@gmail.com>
2025-07-29 18:20:05 +0100

    functions to save and remove movie

--- src/context/BoardContext.tsx
+++ src/context/BoardContext.tsx
@@ -2,11 +2,13 @@ import { createContext, useContext, useEffect, useState } from "react";
 import type { Board } from "../types/board";
 import { v4 as uuid } from "uuid";
 import type { Movie } from "../types/movie";
+import toast from "react-hot-toast";
 
 interface BoardContextType {
     boards: Board[];
     addBoard: (name: string) => void;
     saveMovieToBoard: (boardId: string, movie: Movie) => void;
+    removeMovieFromBoard: (boardId: string, movieId: number) => void;
 }
 
 const BoardContext = createContext<BoardContextType | undefined>(undefined);
@@ -33,21 +35,39 @@ export const BoardProvider = ({ children }: { children: React.ReactNode }) => {
 
     const saveMovieToBoard = (boardId: string, movie: Movie) => {
         setBoards((prevBoards) =>
-            prevBoards.map((board) =>
-                board.id === boardId
-                    ? {
-                        ...board,
-                        movie: board.movies.some((m) => m.id === movie.id)
-                            ? board.movies
-                            : [...board.movies, movie],
-                    }
-                    : board
-            )
+            prevBoards.map((board) => {
+                if (board.id === boardId) return board;
+
+                const alreadyExists = board.movies.some(m => m.id === movie.id);
+                if (alreadyExists) {
+                    toast.error("Movie already added to board.");
+                    return board;
+                };
+
+                toast.success("Movie added to board.");
+                return {
+                    ...board,
+                    movie: [...board.movies, movie]
+                }
+            })
+        );
+    };
+
+    const removeMovieFromBoard = (boardId: string, movieId: number) => {
+        setBoards(prev =>
+            prev.map(board => {
+                if (board.id !== boardId) return board;
+
+                return {
+                    ...board,
+                    movie: board.movies.filter(m => m.id !== movieId)
+                }
+            })
         );
     };
 
     return (
-        <BoardContext.Provider value={{ boards, addBoard, saveMovieToBoard }}>
+        <BoardContext.Provider value={{ boards, addBoard, saveMovieToBoard, removeMovieFromBoard }}>
             {children}
         </BoardContext.Provider>
     );
