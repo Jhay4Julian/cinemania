a8db30a40be3fea2ea71bdf3add53fdf98b7e911
Julian Koripamo <jkoripamo@gmail.com>
2025-08-16 17:25:33 +0100

    added tv shows to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,24 @@ import {
     fetchTrendingMovies,
     fetchTopRatedMovies,
     fetchUpcomingMovies,
-    fetchNowPlayingMovies
+    fetchNowPlayingMovies,
+    fetchPopularShows,
+    fetchTopRatedShows
 } from '../services/movieService';
 import type { Movie } from '../types/movie';
 import MovieSection from '../components/MovieSection';
+import { clsx as cn } from 'clsx';
+import ShowSection from '../components/ShowSection';
+import type { Show } from '../types/show';
 
 const Home = () => {
     const [trending, setTrending] = useState<Movie[]>([]);
     const [topRated, setTopRated] = useState<Movie[]>([]);
     const [upcoming, setUpcoming] = useState<Movie[]>([]);
     const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
+    const [type, setType] = useState<"movie" | "show">("movie");
+    const [popularShow, setPopularShow] = useState<Show[]>([]);
+    const [ratedShow, setRatedShow] = useState<Show[]>([]);
 
     const loadMovies = async () => {
         try {
@@ -32,17 +40,62 @@ const Home = () => {
         }
     };
 
+    const loadShows = async () => {
+        try {
+            const [popularRes, topRatedRes] = await Promise.all([
+                fetchPopularShows(),
+                fetchTopRatedShows(),
+            ]);
+
+            setPopularShow(popularRes.data.results);
+            setRatedShow(topRatedRes.data.results);
+        } catch (error) {
+            console.error('Failed to load tv shows:', error);
+        }
+    }
+
     useEffect(() => {
         loadMovies();
+        loadShows();
     }, []);
+
+
 
     return (
         <div className="p-6 space-y-10">
-            <h1 className='font-bold text-3xl'>Cinemania</h1>
-            <MovieSection title="Trending" movies={trending} />
-            <MovieSection title="Now Playing" movies={nowPlaying} />
-            <MovieSection title="Top Rated" movies={topRated} />
-            <MovieSection title="Upcoming" movies={upcoming} />
+            <div className='flex flex-row justify-center gap-4'>
+                <button
+                    onClick={() => setType("movie")}
+                    className={cn(
+                        "px-4 py-2 rounded cursor-pointer",
+                        type === "movie" ? "bg-blue-600 text-white" : "bg-gray-200"
+                    )}>
+                    Movies
+                </button>
+                <button
+                    onClick={() => setType("show")}
+                    className={cn(
+                        "px-4 py-2 rounded cursor-pointer",
+                        type === "show" ? "bg-blue-600 text-white" : "bg-gray-200"
+                    )}>
+                    Shows
+                </button>
+            </div>
+
+            {type === "movie" ? (
+                <>
+                    <div className='mb-9'></div>
+                    <MovieSection title="Trending" movies={trending} />
+                    {/* <MovieSection title="Now Playing" movies={nowPlaying} /> */}
+                    <MovieSection title="Top Rated" movies={topRated} />
+                    <MovieSection title="Upcoming" movies={upcoming} />
+                </>
+            ) : (
+                <>
+                    <ShowSection title='Popular' shows={popularShow} />
+                    <ShowSection title='Top Rated' shows={ratedShow} />
+                </>
+            )}
         </div>
     );
 };
