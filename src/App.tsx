dd2e0e7f89d4fec53973baa94c213d636b964a48
Julian Koripamo <jkoripamo@gmail.com>
2025-08-16 17:27:11 +0100

    sidebar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
+import BoardPage from './pages/BoardPage'
+import Sidebar from './components/Sidebar'
+import AllBoardsPage from './pages/AllBoardsPage'
+import SearchPage from './pages/SearchPage'
+import ShowDetails from './pages/ShowDetails'
 
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <div className='min-h-screen bg-gray-100 text-gray-900'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/movie/:id' element={<MovieDetails />} />
-          <Route path='*' element={<div className='p-10'>404 - Page Not Found</div>} />
-        </Routes>
+    <Router>
+      {/* <div className='grid grid-cols-[300px_auto] border border-red-400'> */}
+      <div className='flex'>
+        <Sidebar />
+
+        <div className='ml-64 w-full min-h-screen border border-blue-500 bg-gray-100 text-gray-900 overflow-hidden'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/movie/:id' element={<MovieDetails />} />
+            <Route path='/tv/:id' element={<ShowDetails />} />
+            <Route path='/search' element={<SearchPage />} />
+            <Route path='/boards' element={<AllBoardsPage />} />
+            <Route path='/boards/:id' element={<BoardPage />} />
+            <Route path='*' element={<div className='p-10'>404 - Page Not Found</div>} />
+          </Routes>
+        </div>
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
