74c180b9c384122d946ceaab6e4f400da44eb1df
Julian Koripamo <jkoripamo@gmail.com>
2025-08-16 17:28:10 +0100

    show type

---
+++ src/types/show.ts
@@ -0,0 +1,12 @@
+export interface Show {
+    id: number;
+    name: string;
+    number_of_seasons: number;
+    number_of_episodes: number;
+    overview: string;
+    poster_path: string;
+    backdrop_path: string;
+    vote_average: number;
+    first_air_date: string;
+    genres: number[];
+}
